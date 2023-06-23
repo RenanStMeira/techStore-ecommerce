336ebab3e5cdc1ef9a33623135ce6a4c6ba77e51
Renan Meira <renan.meira25@gmail.com>
2023-06-23 20:17:10 -0300

    first commit

---
+++ src/Router/router.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/users");
+
+export default router;
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import router from "./Router/router";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000')
+})
+
