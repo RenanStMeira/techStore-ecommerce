aeb61eaccb1d1a9b114341fbe7a46c0fabd9d1c7
Renan Meira <renan.meira25@gmail.com>
2023-07-06 17:46:57 -0300

    created server.js

---
+++ dist/Service/Prisma/prismaService.d.ts
@@ -0,0 +1,3 @@
+import { PrismaClient } from "@prisma/client";
+export declare const prisma: PrismaClient<import(".prisma/client").Prisma.PrismaClientOptions, never, import(".prisma/client").Prisma.RejectOnNotFound | import(".prisma/client").Prisma.RejectPerOperation>;
+export declare const prismaService: PrismaClient<import(".prisma/client").Prisma.PrismaClientOptions, never, import(".prisma/client").Prisma.RejectOnNotFound | import(".prisma/client").Prisma.RejectPerOperation>;
