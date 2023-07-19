10c25b2558b85a32e85ce622174ee05387e7b105
Renan Meira <renan.meira25@gmail.com>
2023-07-18 21:45:52 -0300

    create cors

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,14 @@ const options: cors.CorsOptions = {
 
 app.use(cors(options));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.use(express.json());
 
 app.use(router);
