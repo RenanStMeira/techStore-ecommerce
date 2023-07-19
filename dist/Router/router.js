b31491a938091ca0bd2199c459ab748aa3ed1ede
Renan Meira <renan.meira25@gmail.com>
2023-07-19 20:50:28 -0300

    build

--- dist/Router/router.js
+++ dist/Router/router.js
@@ -25,6 +25,7 @@ router.post('/admin/create', adminController.createAdmin);
 router.delete('/admin/delete/:id', adminController.deleteAdmin);
 router.post('/admin/login', loginAdminController.loginAdmin);
 router.get('/product/:id', productController.findAll);
+router.get('/product/', productController.findAll);
 router.post('/product/created', productController.create);
 router.delete('/product/delete/:id', productController.delete);
 router.post('/upload/image', uploadMulter.guard('uploads/image'), productController.upload);
