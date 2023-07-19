b681ebf18141750e8ffd1002569cf1956fcca51a
Renan Meira <renan.meira25@gmail.com>
2023-07-18 22:12:06 -0300

    created port 3000

--- dist/server.js
+++ dist/server.js
@@ -10,6 +10,7 @@ const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(router_1.default);
+const port = process.env.PORT || 3000;
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
--- src/server.ts
+++ src/server.ts
@@ -24,6 +24,8 @@ app.use(express.json());
 
 app.use(router);
 
+const port = process.env.PORT || 3000;
+
 
 app.listen(3000, () => {
     console.log('Server running on port 3000')
